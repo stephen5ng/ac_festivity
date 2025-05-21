7bf028d54caede83fd88356d5e4003d905542574
Stephen Ng <stephen5.ng@gmail.com>
2025-05-21 13:52:17 -0700

    start_game.sh

---
+++ start_game.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+if aplay -l 2>&1 | grep -qi "no soundcards found"; then
+    echo "Error: No sound playback devices found." >&2
+    exit 1
+fi
+
+cd /home/dietpi/festivity
+. env/bin/activate
+amixer -c 0 set 'Speaker' 100%
+python ./festivity.py
